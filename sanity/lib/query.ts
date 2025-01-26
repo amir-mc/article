9ee958294b68c77acb5e88a7fdd9902fe809e6d5
Amir_fooladi <34718321+amir-mc@users.noreply.github.com>
2025-01-26 22:57:47 +0330

    Update query.ts

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -1,15 +1,25 @@
 import { defineQuery } from "next-sanity";
 
-export const START_QUERY= defineQuery(`*[_type=="startup" && defined(slug.current)] | order(_creatAt desc){
- _id,
-   slug,
-   title,
-   view,
-   image,
-   author->{_id,name,bio,image},
-   category,
-   description,
-_createdAt,
-   pitch
-}
-`)
+export const START_QUERY = defineQuery(`
+  *[
+    _type == "startup" && 
+    defined(slug.current) && 
+    (
+      !defined($search) || 
+      category match $search || 
+      author->name match $search || 
+      title match $search
+    )
+  ] | order(_createdAt desc) {
+    _id,
+    slug,
+    title,
+    view,
+    image,
+    author->{_id, name, bio, image},
+    category,
+    description,
+    _createdAt,
+    pitch
+  }
+`);
